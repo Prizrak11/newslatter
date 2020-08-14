1ce48a2f57ea050b853f854baf6dae8079fc5842
Prizrak <santipato2004@gmail.com>
2020-08-14 16:39:16 -0500

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import '../sass/main.scss'
+//Images
+import '../static/bg_instagram.jpg'
+//Babel
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+//JavaScript
+require('../js/route.js')
